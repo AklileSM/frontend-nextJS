b420abc6ab9cd1ec78f520b1086e0af2308fdf8b
AklileSM <atm9561@nyu.edu>
2026-05-12 12:43:17 +0400

    Refactor LoginClientIsland and RegisterClientIsland components to remove AuthPageProviders, simplifying the structure by directly rendering LoginForm and RegisterForm.

--- components/auth/LoginClientIsland.tsx
+++ components/auth/LoginClientIsland.tsx
@@ -2,20 +2,11 @@
 
 import dynamic from 'next/dynamic';
 
-const AuthPageProviders = dynamic(
-  () => import('@/components/providers/RouteProviders').then((mod) => mod.AuthPageProviders),
-  { ssr: false },
-);
-
 const LoginForm = dynamic(
   () => import('@/components/auth/LoginForm').then((mod) => mod.LoginForm),
   { ssr: false },
 );
 
 export function LoginClientIsland({ next }: { next?: string }) {
-  return (
-    <AuthPageProviders>
-      <LoginForm next={next} />
-    </AuthPageProviders>
-  );
+  return <LoginForm next={next} />;
 }
--- components/auth/RegisterClientIsland.tsx
+++ components/auth/RegisterClientIsland.tsx
@@ -2,20 +2,11 @@
 
 import dynamic from 'next/dynamic';
 
-const AuthPageProviders = dynamic(
-  () => import('@/components/providers/RouteProviders').then((mod) => mod.AuthPageProviders),
-  { ssr: false },
-);
-
 const RegisterForm = dynamic(
   () => import('@/components/auth/RegisterForm').then((mod) => mod.RegisterForm),
   { ssr: false },
 );
 
 export function RegisterClientIsland() {
-  return (
-    <AuthPageProviders>
-      <RegisterForm />
-    </AuthPageProviders>
-  );
+  return <RegisterForm />;
 }
